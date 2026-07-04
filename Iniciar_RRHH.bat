14b31c95d9b4d01c33074b2232ae32579fe69fa1
Axel828388 <axeljaviersilvabarahona@gmail.com>
2026-07-04 17:19:18 -0600

    Add RRHH startup batch script

---
+++ Iniciar_RRHH.bat
@@ -0,0 +1,13 @@
+@echo off
+title Servidor RRHH
+
+cd /d "%~dp0backend"
+
+echo ===============================
+echo      INICIANDO BACKEND RRHH
+echo ===============================
+echo.
+
+npm start
+
+pause
